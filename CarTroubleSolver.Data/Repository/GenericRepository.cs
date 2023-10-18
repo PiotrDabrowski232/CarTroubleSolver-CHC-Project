c860e5dd4625c71370f492587b25de467cd08a94
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-10-18 21:52:00 +0200

    Generic Repo Added

---
+++ CarTroubleSolver.Data/Repository/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using CarTroubleSolver.Data.Repository.Interfaces;
+
+namespace CarTroubleSolver.Data.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CarTroubleSolver.Data/Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTroubleSolver.Data.Repository.Interfaces
+{
+    internal interface IGenericRepository<T>
+    {
+        public Task Add(T entity);
+        public T Get(Guid id);
+        public void Update(T entity);
+        public void Remove(T entity);
+        public IEnumerable<T> GetAll();
+    }
+}

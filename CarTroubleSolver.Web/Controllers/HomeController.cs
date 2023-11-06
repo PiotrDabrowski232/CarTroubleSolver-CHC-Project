d1320a1ff5b0646008fa42b0f2e7dd28278cb16e
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-11-06 12:20:15 +0100

    adding register page

--- CarTroubleSolver.Web/Controllers/HomeController.cs
+++ CarTroubleSolver.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CarTroubleSolver.Web.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return RedirectToAction("Register");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

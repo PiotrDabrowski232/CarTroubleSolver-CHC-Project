c3fed6daba5f01ad3e132b0f928021d8cea64c4d
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-11-09 10:48:32 +0100

    adding car to database now for one user

--- CarTroubleSolver.Logic/Services/Interfaces/ICarService.cs
+++ CarTroubleSolver.Logic/Services/Interfaces/ICarService.cs
@@ -10,7 +10,7 @@ namespace CarTroubleSolver.Logic.Services.Interfaces
 {
     public interface ICarService
     {
-        public void Add(CarDto user, string userEmail);
+        public void Add(CarDto car, string userEmail);
         public void DeleteCarFromUserCollection(CarDto carToDelete, string userEmail);
         public IEnumerable<CarDto> GetUserCars(string userEmail);
         public Guid GetCarId(CarDto car, string userEmail);
--- CarTroubleSolver.Web/Controllers/CarController.cs
+++ CarTroubleSolver.Web/Controllers/CarController.cs
@@ -1,10 +1,17 @@
 ﻿using CarTroubleSolver.Logic.Dto.Cars;
+using CarTroubleSolver.Logic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarTroubleSolver.Web.Controllers
 {
     public class CarController : Controller
     {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,9 +19,9 @@ namespace CarTroubleSolver.Web.Controllers
 
         public IActionResult AddCar(CarDto car)
         {
-
             if (ModelState.IsValid)
             {
+                _carService.Add(car, "pp@o2.pl");
                 return RedirectToAction("Index", "Home");
             }
             return View("Index", car);

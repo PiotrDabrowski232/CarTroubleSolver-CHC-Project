0962f285f607ba722e18af0a46167ebd71f13a92
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-11-24 13:45:52 +0100

    accident request styled

--- CarTroubleSolver.Web/obj/Debug/net6.0/CarTroubleSolver.Web.AssemblyInfo.cs
+++ CarTroubleSolver.Web/obj/Debug/net6.0/CarTroubleSolver.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6143d230b40ca5f3365df3dc0db4a712915915e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f0513ca5f89ee3ee47bc880ec20defee702dd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

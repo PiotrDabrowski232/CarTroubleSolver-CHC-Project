f1bbd943f1b56d4cab3c08aa9fa5e1b8cdf4a472
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-11-24 14:06:34 +0100

    last changes

--- CarTroubleSolver.Web/obj/Debug/net6.0/CarTroubleSolver.Web.AssemblyInfo.cs
+++ CarTroubleSolver.Web/obj/Debug/net6.0/CarTroubleSolver.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5425f0d1c1caeba7d34c851e5888f4278451288f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarTroubleSolver.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
